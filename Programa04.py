8f950ea1b56ee94df9892b0040037989dd623f59
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-17 20:34:05 -0300

    Resolucao exe 04

---
+++ Programa04.py
@@ -0,0 +1,28 @@
+"""
+No SENAILÂNDIA mulheres e homens podem servir o exército do país. O serviço é opcional e é muito comum que as pessoas se apresentem para o serviço em algum momento da vida.
+ Existe uma única restrição para o ingresso, que é a idade da pessoa: 
+•	Para mulheres, a idade aceita é entre 21 e 34 anos (>=21 e <=34)
+•	Para homens, a idade aceita é entre 18 e 39 anos (>=18 e <=39)
+Escreva um programa que leia 3 dados de entrada: Nome da pessoa, idade e gênero. Informe se a pessoa será aceita ou não para o serviço.
+	OBS: Para o gênero deve ser lido somente um caractere, que pode ser: ‘f’ ou ‘F’, ‘m’ ou ‘M’. Qualquer coisa diferente, deve ser informado como inválido.
+"""
+
+nomeUsuario = input("Digite seu nome: ")
+idadeUsuario = int(input("Digite sua idade: "))
+generoUsuario = input("Digite seu gênero [m/f]: ").lower()
+
+if generoUsuario.__eq__("m"):
+    if idadeUsuario >= 18 and idadeUsuario <= 39:
+        isAceito = "ACEITO!"
+    else:
+        isAceito = "NÂO ACEITO!"
+
+elif generoUsuario.__eq__("f"):
+    if idadeUsuario >= 21 and idadeUsuario <= 34:
+        isAceito = "ACEITO!"
+    else:
+        isAceito = "NÂO ACEITO!"
+else:
+    print("Gênero inserido não aceito!")
+
+print("\nNome informado: {}\nIdade informada: {}\nGênero informado: {}\nAceito ao exército: {}".format(nomeUsuario, idadeUsuario, generoUsuario, isAceito))
