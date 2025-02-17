32688ca46c45d1843ef2f800dc27dd05ab22a8aa
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-17 19:33:51 -0300

    Edicao pro1 e 2

--- Programa01.py
+++ Programa01.py
@@ -1,3 +1,9 @@
+"""
+Escreva um programa que forneça um tipo de aplicação financeira adequado a um investidor a partir de 2 dados fornecidos: O grau de aceitação de risco e o valor a ser investido.
+Deve ser lido no teclado na forma BX ou AL risco. Se for fornecido algo diferente disso, o programa deve mostrar uma mensagem indicando que foi fornecido um dado inválido.
+Para o valor, deve ser um número real.
+"""
+
 grauRisco = input("Digite o grau de risco desejado ['BX' ou 'AL']: ").casefold()
 if grauRisco != 'BX' or 'AL':
     print("Grau de risco não identificado!")
--- Programa02.py
+++ Programa02.py
@@ -1,5 +1,32 @@
+"""
+Escreva um programa para exibir na tela o nome e a categoria de um lutador, o programa deve ler uma string para o nome e um número real para o peso.
+"""
+
 nomeLutador = input("Digite o nome do lutador: ")
 pesoLutador = float(input("Digite o peso do lutador: "))
 
 if pesoLutador < 52:
     categoria = "invalido"
+
+elif pesoLutador >= 52 and pesoLutador < 65:
+    categoria = "Peso Pena"
+
+elif pesoLutador >= 65 and pesoLutador < 72:
+    categoria = "Peso Leve"
+
+elif pesoLutador >= 72 and pesoLutador < 79:
+    categoria = "Peso Ligeiro"
+
+elif pesoLutador >= 79 and pesoLutador < 86:
+    categoria = "Peso Meio-Médio"
+
+elif pesoLutador >= 52 and pesoLutador < 65:
+    categoria = "Peso Médio"
+
+elif pesoLutador >= 52 and pesoLutador < 65:
+    categoria = "Peso Meio-Pesado"
+
+else:
+    categoria = "Peso Pesado"
+
+    
